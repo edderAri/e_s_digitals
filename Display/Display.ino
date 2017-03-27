3e223c46d60874db52109d90675f3eb6af004c36
edderAri <edder.1022@gmail.com>
2017-03-27 19:36:19 +0200

    2

--- Display/Display.ino
+++ Display/Display.ino
@@ -18,7 +18,8 @@ const int ledF = 10;
 const int ledG = 11;
 int pausa = 500;
 const int polsador = 2;
-boolean polsadorEstat = LOW
+boolean polsadorEstat = LOW; // variable estat  polsador
+int num = 0; // variale numero a mostrar
 
 //************************* SETUP *******************************
 
@@ -33,7 +34,7 @@ void setup() {               // configura el final de salida
   pinMode(ledG, OUTPUT);
   pinMode(polsador, INPUT);
 
-   //tot parat
+  //tot parat
   digitalWrite(ledA, LOW);
   digitalWrite(ledB, LOW);
   digitalWrite(ledC, LOW);
@@ -47,131 +48,118 @@ void setup() {               // configura el final de salida
 //************************** LOOP *******************************
 
 void loop() {                // inicia el bucle del programa
-    
-   delay(pausa);
-   polsadorEstat = digitalRead(polsador);
-   if (polsadorEstat == HIGH)
-   {
+
+  delay(pausa);
+  polsadorEstat = digitalRead(polsador);
+  if (polsadorEstat == HIGH)
+  {
     num = num + 1; //incrementar numero a mostar
     if (num == 10) num = 0;  //despres del 9 pasa a 0
-   }
-    switch (num)
-   {
+  }
+  switch (num)
+  {
     case 0:
+      //numero 0
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, HIGH);
+      digitalWrite(ledE, HIGH);
+      digitalWrite(ledF, HIGH);
+      digitalWrite(ledG, LOW);
+      break;
+    case 1:
 
-    //numero 0
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, HIGH);
-  digitalWrite(ledE, HIGH);
-  digitalWrite(ledF, HIGH);
-  digitalWrite(ledG, LOW);
-  break;
- case 1:  
-    
-    //numero 1
-  digitalWrite(ledA, LOW);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, LOW);
-  digitalWrite(ledE, LOW);
-  digitalWrite(ledF, LOW);
-  digitalWrite(ledG, LOW);
-  break;
- case 2:
-
-    //numero 2
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, LOW);
-  digitalWrite(ledD, HIGH);
-  digitalWrite(ledE, HIGH);
-  digitalWrite(ledF, LOW);
-  digitalWrite(ledG, HIGH);
-  break;
- case 3:
-
-    //numero 3
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, HIGH);
-  digitalWrite(ledE, LOW);
-  digitalWrite(ledF, LOW);
-  digitalWrite(ledG, HIGH);
-
-  delay(pausa);
-
-    //numero 4
-  digitalWrite(ledA, LOW);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, LOW);
-  digitalWrite(ledE, LOW);
-  digitalWrite(ledF, HIGH);
-  digitalWrite(ledG, HIGH);
-
-  delay(pausa);
-
-    // numero 5
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, LOW);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, HIGH);
-  digitalWrite(ledE, LOW);
-  digitalWrite(ledF, HIGH);
-  digitalWrite(ledG, HIGH);
-
-  delay(pausa);
-
-    // numero 6
-  digitalWrite(ledA, LOW);
-  digitalWrite(ledB, LOW);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, HIGH);
-  digitalWrite(ledE, HIGH);
-  digitalWrite(ledF, HIGH);
-  digitalWrite(ledG, HIGH);
-
-  delay(pausa);
-
-    // numero 7
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, LOW);
-  digitalWrite(ledE, LOW);
-  digitalWrite(ledF, LOW);
-  digitalWrite(ledG, LOW);
-
-  delay(pausa);
-
-    // numero 8
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, HIGH);
-  digitalWrite(ledE, HIGH);
-  digitalWrite(ledF, HIGH);
-  digitalWrite(ledG, HIGH);
-
-  delay(pausa);
-
-    // numero 9
-  digitalWrite(ledA, HIGH);
-  digitalWrite(ledB, HIGH);
-  digitalWrite(ledC, HIGH);
-  digitalWrite(ledD, LOW);
-  digitalWrite(ledE, LOW);
-  digitalWrite(ledF, HIGH);
-  digitalWrite(ledG, HIGH);
-
-  delay(pausa);
-  
-  
-  
-  
+      //numero 1
+      digitalWrite(ledA, LOW);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, LOW);
+      digitalWrite(ledE, LOW);
+      digitalWrite(ledF, LOW);
+      digitalWrite(ledG, LOW);
+      break;
+    case 2:
+      //numero 2
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, LOW);
+      digitalWrite(ledD, HIGH);
+      digitalWrite(ledE, HIGH);
+      digitalWrite(ledF, LOW);
+      digitalWrite(ledG, HIGH);
+      break;
+    case 3:
+      //numero 3
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, HIGH);
+      digitalWrite(ledE, LOW);
+      digitalWrite(ledF, LOW);
+      digitalWrite(ledG, HIGH);
+      break;
+    case 4:
+      //numero 4
+      digitalWrite(ledA, LOW);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, LOW);
+      digitalWrite(ledE, LOW);
+      digitalWrite(ledF, HIGH);
+      digitalWrite(ledG, HIGH);
+      break;
+    case 5:
+      // numero 5
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, LOW);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, HIGH);
+      digitalWrite(ledE, LOW);
+      digitalWrite(ledF, HIGH);
+      digitalWrite(ledG, HIGH);
+      break;
+    case 6:
+      // numero 6
+      digitalWrite(ledA, LOW);
+      digitalWrite(ledB, LOW);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, HIGH);
+      digitalWrite(ledE, HIGH);
+      digitalWrite(ledF, HIGH);
+      digitalWrite(ledG, HIGH);
+      break;
+    case 7:
+      // numero 7
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, LOW);
+      digitalWrite(ledE, LOW);
+      digitalWrite(ledF, LOW);
+      digitalWrite(ledG, LOW);
+      break;
+    case 8:
+      // numero 8
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, HIGH);
+      digitalWrite(ledE, HIGH);
+      digitalWrite(ledF, HIGH);
+      digitalWrite(ledG, HIGH);
+      break;
+    case 9:
+      // numero 9
+      digitalWrite(ledA, HIGH);
+      digitalWrite(ledB, HIGH);
+      digitalWrite(ledC, HIGH);
+      digitalWrite(ledD, LOW);
+      digitalWrite(ledE, LOW);
+      digitalWrite(ledF, HIGH);
+      digitalWrite(ledG, HIGH);
+      break;
+  }
 }
 
 //************************* FUNCIONS ****************************
